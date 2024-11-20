c59a0a69d660a841ae0ef524287ac07b8ed1891b
Glazkov-Kostya <JIaIIaJIyT@gmail.com>
2024-11-21 00:12:39 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,5 @@
+the_number_of_completed_DZ = 12
+numberOfHoursSpent = 1.5
+course_name = 'Python'
+time_for_one_task = numberOfHoursSpent/the_number_of_completed_DZ
+print('Курс: ',course_name,', всего задач: ',the_number_of_completed_DZ,', затрачено часов: ',numberOfHoursSpent,', среднее время выполнения:',time_for_one_task)
